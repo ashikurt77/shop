0c62c5c60150109c25eb1e8da9453757e7d1c199
Ashiqur Tusher <ashikurt77@gmail.com>
2019-11-20 13:08:43 +0600

    lesson 2

---
+++ src/components/menu-item/MenuItem.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./MenuItem.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  return (
+    <div
+      className={`menu-item ${size}`}
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    >
+      <div className="content">
+        <h1 className="title"> {title}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
